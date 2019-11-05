b3918baa3ee216f0369625721cbf1ebf078ef166
Sematre <Sematre@gmx.de>
2019-11-05 22:13:56 +0100

    Make UUID random

--- src/main/java/de/sematre/dsbmobile/DSBMobile.java
+++ src/main/java/de/sematre/dsbmobile/DSBMobile.java
@@ -37,7 +37,7 @@ public class DSBMobile implements Serializable, Cloneable {
 		args.put("Language", "de");
 
 		args.put("Device", "Nexus 4");
-		args.put("AppId", "414c8312-bbac-4274-b5f4-8bbcfb613580");
+		args.put("AppId", UUID.randomUUID().toString());
 		args.put("AppVersion", "2.5.9");
 		args.put("OsVersion", "27 8.1.0");
 
